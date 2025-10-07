b7b32d7a923dd7286dd600c22acee5653672a4be
Abe Kline <abekline@gmail.com>
2025-10-07 13:45:02 -0500

    Rename SBg.bat to Archive/SBg.bat

---
+++ Archive/SBg.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Git\bin\bash.exe" --login -i -- "C:\.git\SpaceBackground\SpaceBackground.sh"
--- SBg.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\Git\bin\bash.exe" --login -i -- "C:\.git\SpaceBackground\SpaceBackground.sh"
